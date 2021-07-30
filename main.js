5f30a2dcbffddedb9ed27cc1b9d98d3098694d14
Ishithareddy123 <81242657+Ishithareddy123@users.noreply.github.com>
2021-07-30 17:14:30 +0530

    Update main.js

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ function submit()
     var lenght_of_name = names_of_people.length;
     console.log(lenght_of_name);
 	document.getElementById("display_name").innerHTML=names_of_people.toString();
-	names_of_people.push(GuestName);
+	
    }
 
 
